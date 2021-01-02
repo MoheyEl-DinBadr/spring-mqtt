9ce0ac88997124769b33dbe9da17b1f88c6813af
MoheyEl-DinBadr <MoheyEl-DinBadr@users.noreply.github.com>
2021-01-02 14:46:38 +0200

    MEB: adding hash, equals method and not null to topic and qos

--- src/main/java/com/mohey/mqtt/model/SubscribedTuple.java
+++ src/main/java/com/mohey/mqtt/model/SubscribedTuple.java
@@ -1,6 +1,7 @@
 package com.mohey.mqtt.model;
 
 import lombok.Getter;
+import lombok.NonNull;
 import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 
 /**
@@ -10,8 +11,10 @@ import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
 @Getter
 public class SubscribedTuple {
 
+    @NonNull
     private String topic;
 
+    @NonNull
     private int qos;
 
     private IMqttMessageListener messageListener;
@@ -20,5 +23,25 @@ public class SubscribedTuple {
         this.topic = topic;
         this.qos = qos;
         this.messageListener = messageListener;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SubscribedTuple tuple = (SubscribedTuple) o;
+
+        if (qos != tuple.qos) return false;
+        if (!topic.equals(tuple.topic)) return false;
+        return messageListener != null ? messageListener.equals(tuple.messageListener) : tuple.messageListener == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = topic.hashCode();
+        result = 31 * result + qos;
+        result = 31 * result + (messageListener != null ? messageListener.hashCode() : 0);
+        return result;
     }
 }
