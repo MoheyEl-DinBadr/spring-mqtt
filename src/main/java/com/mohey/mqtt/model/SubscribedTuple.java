a251467bafcde7cab2864b00ead8fa80782f1bb4
MoheyEl-DinBadr <MoheyEl-DinBadr@users.noreply.github.com>
2021-01-30 15:25:22 +0200

    MEB: made SubscribedTuple extends MqttSubscription to add new related feature soon

--- src/main/java/com/mohey/mqtt/model/SubscribedTuple.java
+++ src/main/java/com/mohey/mqtt/model/SubscribedTuple.java
@@ -5,41 +5,41 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.eclipse.paho.mqttv5.client.IMqttMessageListener;
+import org.eclipse.paho.mqttv5.common.MqttSubscription;
 
 /**
  * @author Mohey El-Din Badr
  * @date 2021/1/1
  */
 @Getter
-@AllArgsConstructor
-@RequiredArgsConstructor
-public class SubscribedTuple {
-
-    @NonNull
-    private String topic;
-
-    @NonNull
-    private int qos;
+public class SubscribedTuple extends MqttSubscription {
 
     private IMqttMessageListener messageListener;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        SubscribedTuple tuple = (SubscribedTuple) o;
-
-        if (qos != tuple.qos) return false;
-        if (!topic.equals(tuple.topic)) return false;
-        return messageListener != null ? messageListener.equals(tuple.messageListener) : tuple.messageListener == null;
+    /**
+     * Constructs a subscription with the specified topic with
+     * all other values set to defaults.
+     * <p>
+     * The defaults are:
+     * <ul>
+     * 	<li>Subscription QoS is set to 1</li>
+     * 	<li>Messages published to this topic by the same client will also be received.</li>
+     * 	<li>Messages received by this subscription will keep the retain flag (if it is set).</li>
+     * 	<li>Retained messages on this topic will be delivered once the subscription has been made.</li>
+     * </ul>
+     *
+     * @param topic The Topic
+     */
+    private SubscribedTuple(String topic) {
+        super(topic);
     }
 
-    @Override
-    public int hashCode() {
-        int result = topic.hashCode();
-        result = 31 * result + qos;
-        result = 31 * result + (messageListener != null ? messageListener.hashCode() : 0);
-        return result;
+    public SubscribedTuple(String topic, int qos) {
+        super(topic, qos);
+    }
+
+    public SubscribedTuple(String topic, int qos, IMqttMessageListener messageListener){
+        super(topic, qos);
+        this.messageListener = messageListener;
     }
 }
